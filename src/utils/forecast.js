fee3a2ebc8eb58940a7a3a97fea86dbb5e77f738
Mustakim <thisismustakim@gmail.com>
2019-05-30 18:25:23 +0600

    Change in forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/df5742cbed3ef9e782341fbdc7f9aa4c/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/df5742cbed3ef9e782341fbdc7f9aa4c/' + latitude + ',' + longitude+'?units=si'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out.The high today is '+body.daily.data[0].temperatureHigh+'with a low of '+body.daily.data[0].temperatureLow+'. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
